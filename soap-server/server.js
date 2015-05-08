bd1172434392e4cbc5f1ababa01c19e1deb4aff1
martinacd08 <casandra123>
2015-05-07 19:06:33 -0700

    7

--- soap-server/server.js
+++ soap-server/server.js
@@ -17,4 +17,4 @@ var soapServer = new soap.SoapServer();
 var soapService = soapServer.addService('testService', new MyTestService());
 var soapService2 = soapServer.addService('testService2', new MyTestService2());
     
-soapServer.listen(1337, 'https://fathomless-shelf-3782.herokuapp.com');
+soapServer.listen(1337, '127.0.0.1');
