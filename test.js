59ec51293c0222d54e7e9401367b2d0a0f9815b9
martinacd08 <casandra123>
2015-05-26 12:48:47 -0700

    cambio

--- test.js
+++ test.js
@@ -18,8 +18,8 @@ function MyTestService(){
 MyTestService.prototype.test1 = function(myArg1, myArg2){
 	return myArg1 + myArg2;
 };
-MyTestService.prototype.test2 = function(myArg1, myArg2){
-	return myArg1 + myArg2;
+MyTestService.prototype.test2 = function(){
+	return 2002;
 };
 MyTestService.prototype.test3 = function(strArg, intArg){
 	var ret = new MyObject();
@@ -36,8 +36,7 @@ var soapService = soapServer.addService('testService', new MyTestService());
 
 var test2operation = soapService.getOperation('test2');
 test2operation.setOutputType('number');
-test2operation.setInputType('myArg1', {type: 'number'});
-test2operation.setInputType('myArg2', {type: 'number'});
+
 
 var test3operation = soapService.getOperation('test3');
 test3operation.setOutputType(MyObject, 'MyObject');
